bb981eb4e083b3f11221ceb91d79399914b6cd29
MADHUMITHA-140607 <2420030205@klh.edu.in>
2025-10-08 08:39:27 +0530

    Add files via upload

---
+++ week 7/data.js
@@ -0,0 +1,6 @@
+export const books = [
+  { title: "Harry Potter and the Philosopher's Stone", author: "JK Rowling", price: 1499, availability: "in stock" },
+  { title: "Harry Potter and the Chamber of Secrets", author: "JK Rowling", price: 1699, availability: "in stock" },
+  { title: "Harry Potter and the Prisoner of Azkaban", author: "JK Rowling", price: 1599, availability: "out of stock" },
+  { title: "Harry Potter and the Goblet of Fire", author: "JK Rowling", price: 1450, availability: "in stock" }
+];
